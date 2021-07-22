566dd0e433566daf88d6ae0d289412410458428f
Martin <martin.b.meier@gmail.com>
2021-07-22 14:55:08 -0500

    updates

--- plugins/gen_match.cc
+++ plugins/gen_match.cc
@@ -254,9 +254,9 @@ for(int ii=0; ii<4; ii++){
 		const reco::GenParticle* lepton2   = 0;
 
 		for (std::vector<reco::GenParticle>::const_iterator iParticle = genParticles->begin(); iParticle != genParticles->end(); iParticle++) {
-			//if( ! iParticle->isHardProcess() ){ continue; }  //ONLY HARD PROCESS AND NOT INCOMING
-			if(!tWfinder(iEvent, &(*iParticle))){ continue; }		//Only final state muons and electrons from t->W
+			if( ! iParticle->isHardProcess() ){ continue; }  //ONLY HARD PROCESS AND NOT INCOMING
 			if( abs( iParticle->pdgId() ) == 13 || abs( iParticle->pdgId() ) == 11 || abs( iParticle->pdgId() ) == 15) {//HERE'S A LEPtON
+				if(!tWfinder(iEvent, &(*iParticle))){ continue; }		//Only final state muons and electrons from t->W
 				if(fabs(iParticle->eta()) > 2.4 || iParticle->pt() < 10){
 					if(lepton1Cuts==0){
 						lepton1Cuts=1;
