71a8903216a4dff40e1d79f024564b138c8dfbdc
Martin <martin.b.meier@gmail.com>
2021-09-07 10:28:39 -0500

    updates

--- plugins/cut_flow2.cc
+++ plugins/cut_flow2.cc
@@ -630,9 +630,9 @@ cut_flow2::beginJob() {
 
 	std::ofstream myfile;
 
-	myfile.open("neuralNetData.csv",std::ios_base::app);
-	myfile<<"muon phi, muon eta, electron pt, electron phi, electron eta, jet 1 pt, jet 1 phi, jet 1 eta, jet 2 pt, jet 2 phi, jet 2 eta, combined jets pt, combined jets phi, combined jets eta, combined jets mass, MET pt, MET phi, gen electron pt, gen muon pt, gen muon/electron pt ratio\n";
-	myfile.close();
+	// myfile.open("neuralNetData.csv",std::ios_base::app);
+	// myfile<<"muon phi, muon eta, electron pt, electron phi, electron eta, jet 1 pt, jet 1 phi, jet 1 eta, jet 2 pt, jet 2 phi, jet 2 eta, combined jets pt, combined jets phi, combined jets eta, combined jets mass, MET pt, MET phi, gen electron pt, gen muon pt, gen muon/electron pt ratio\n";
+	// myfile.close();
 
 	edm::Service<TFileService> fs; 
 
