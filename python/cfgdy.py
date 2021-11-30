b10a6eff0799abb540eb3a4388088275335ee535
Martin <martin.b.meier@gmail.com>
2021-11-30 11:23:15 -0600

    updates

--- python/cfgdy.py
+++ python/cfgdy.py
@@ -104,12 +104,13 @@ process.source = cms.Source ("PoolSource",
       fileNames = cms.untracked.vstring (
 
         #WR1600_N800
-'file:/home/meier307/MINIAOD_5.root',
-'file:/home/meier307/MINIAOD_4.root',
-'file:/home/meier307/MINIAOD_3.root',
-'file:/home/meier307/MINIAOD_2.root',
-'file:/home/meier307/MINIAOD_1.root',
-'file:/home/meier307/MINIAOD_28.root',
+'file:/home/meier307/MINIAOD_6.root',
+'file:/home/meier307/MINIAOD_7.root',
+'file:/home/meier307/MINIAOD_8.root',
+'file:/home/meier307/MINIAOD_9.root',
+'file:/home/meier307/MINIAOD_10.root',
+'file:/home/meier307/MINIAOD_12.root',
+'file:/home/meier307/MINIAOD_13.root',
 
         #ttbar
 # 'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/079C3FC4-8835-394B-8E54-1C67DFAE7E8D.root',
