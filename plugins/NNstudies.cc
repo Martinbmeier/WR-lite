eff7346e318a6674d93e853354d05d598f9c0308
Martin <martin.b.meier@gmail.com>
2022-03-24 08:45:25 -0500

    updates

--- plugins/NNstudies.cc
+++ plugins/NNstudies.cc
@@ -530,10 +530,12 @@ math::XYZTLorentzVector genMuonP4 = genMuon->p4();
 math::XYZTLorentzVector genElectronP4 = genElectron->p4();
 math::XYZTLorentzVector muNuP4 = muNu->p4();
 math::XYZTLorentzVector eNuP4 = eNu->p4();
+math::XYZTLorentzVector tquarkP4 = tquark->p4();
+math::XYZTLorentzVector antitquarkP4 = antitquark->p4();
 
 
 std::ofstream myfile;
-myfile.open("neuralNetDataTT_2.csv",std::ios_base::app);
+myfile.open("neuralNetDataTT_1.csv",std::ios_base::app);
 myfile << muonP4.Px() << ", "
 		   << muonP4.Py() << ", "
        << muonP4.Pz() << ", "
@@ -573,7 +575,15 @@ myfile << muonP4.Px() << ", "
        << eNuP4.Px() << ", "
        << eNuP4.Py() << ", "
        << eNuP4.Pz() << ", "
-       << eNuP4.E()  << "\n ";
+       << eNuP4.E()  << ", "
+       << antitquarkP4.Px() << ", "
+       << antitquarkP4.Py() << ", "
+       << antitquarkP4.Pz() << ", "
+       << antitquarkP4.E() << ", "
+       << tquarkP4.Px() << ", "
+       << tquarkP4.Py() << ", "
+       << tquarkP4.Pz() << ", "
+       << tquarkP4.E() << "\n ";
 
 
 myfile.close();
@@ -587,9 +597,9 @@ NNstudies::beginJob() {
 
 	std::ofstream myfile;
 
-	// myfile.open("neuralNetDataTT_1.csv",std::ios_base::app);
-	// myfile<<"muonP1, muonP2, muonP3, muonP4, electronP1, electronP2, electronP3, electronP4, bjetP1, bjetP2, bjetP3, bjetP4, jetP1, jetP2, jetP3, jetP4, combinedJetsP1, combinedJetsP2, combinedJetsP3, combinedJetsP4, MET pt, MET phi, event weight, bin number, genMuonP1, genMuonP2, genMuonP3, genMuonP4, genElectronP1, genElectronP2, genElectronP3, genElectronP4, muNuP1, muNuP2, muNuP3,muNuP4 eNu1, eNuP2, eNuP3, eNuP4 \n";
-	// myfile.close();
+	myfile.open("neuralNetDataTT_1.csv",std::ios_base::app);
+	myfile<<"muonP1,muonP2,muonP3,muonP4,electronP1,electronP2,electronP3,electronP4,bjetP1,bjetP2,bjetP3,bjetP4,jetP1,jetP2,jetP3,jetP4,combinedJetsP1,combinedJetsP2,combinedJetsP3,combinedJetsP4,METpt,METphi,eventWeight,binNumber,genMuonP1,genMuonP2,genMuonP3,genMuonP4,genElectronP1,genElectronP2,genElectronP3,genElectronP4,muNuP1,muNuP2,muNuP3,muNuP4,eNuP1,eNuP2,eNuP3,eNuP4,antitquarkP1,antitquarkP2,antitquarkP3,antitquarkP4,tquarkP1,tquarkP12,tquarkP3,tquarkP4\n";
+	myfile.close();
 
 	edm::Service<TFileService> fs;
 
