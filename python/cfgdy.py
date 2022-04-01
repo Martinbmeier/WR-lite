c1fda763c87d7a495710603365b5ea1a87404149
Martin <martin.b.meier@gmail.com>
2022-03-31 22:18:18 -0500

    updates

--- python/cfgdy.py
+++ python/cfgdy.py
@@ -371,7 +371,7 @@ elif options.era == '2018':
 # )
 
 process.muonpPtFilter = cms.EDFilter("muonFilter",
-      ptMin = cms.double(200),
+      ptMin = cms.double(300),
       genParticles = cms.InputTag("prunedGenParticles"),
 )
 
