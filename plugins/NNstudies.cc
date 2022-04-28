304abe6c74e1500ab926594c5f55f245af80f15e
Martin <martin.b.meier@gmail.com>
2022-04-27 22:55:10 -0500

    updates

--- plugins/NNstudies.cc
+++ plugins/NNstudies.cc
@@ -560,7 +560,7 @@ math::XYZTLorentzVector antitquarkP4 = antitquark->p4();
 
 
 std::ofstream myfile;
-myfile.open("neuralNetDataTT_3.csv",std::ios_base::app);
+myfile.open("neuralNetDataTT_4.csv",std::ios_base::app);
 myfile << muonP4.Px() << ", "
 		   << muonP4.Py() << ", "
        << muonP4.Pz() << ", "
