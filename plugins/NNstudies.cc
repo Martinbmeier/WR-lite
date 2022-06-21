5118e4a3412ad439c9628a835b133f35be439420
Martin <martin.b.meier@gmail.com>
2022-06-21 16:14:25 -0500

    updates

--- plugins/NNstudies.cc
+++ plugins/NNstudies.cc
@@ -578,7 +578,7 @@ math::XYZTLorentzVector metP4 = Met.p4();
 
 
 std::ofstream myfile;
-myfile.open("neuralNetDataTTID_2.csv",std::ios_base::app);
+myfile.open("neuralNetDataTTID_3.csv",std::ios_base::app);
 myfile << muonP4.Px() << ", "
 		   << muonP4.Py() << ", "
        << muonP4.Pz() << ", "
