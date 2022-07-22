88e2073a781c5543502aac3808194d3895b7bfe9
Martin <martin.b.meier@gmail.com>
2022-07-22 13:04:29 -0500

    updates

--- python/cfgdy.py
+++ python/cfgdy.py
@@ -689,7 +689,7 @@ process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
                                    printP4 = cms.untracked.bool(False),
                                    printPtEtaPhi = cms.untracked.bool(False),
                                    printVertex = cms.untracked.bool(False),
-                                   printStatus = cms.untracked.bool(True),
+                                   printStatus = cms.untracked.bool(False),
                                    printIndex = cms.untracked.bool(False),
                                    #status = cms.untracked.vint32( 3 )
                                    )
